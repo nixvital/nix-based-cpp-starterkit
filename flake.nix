93fc3c8a2d27337d817f9405cbfb4e63d76fa5da
Break Yang <breakds@gmail.com>
2022-12-27 13:41:28 -0800

    Upgrade to 22.11

--- flake.nix
+++ flake.nix
@@ -9,56 +9,56 @@
     # E.g.
     #
     # nixpkgs.url = "github:NixOS/nixpkgs/unstable";
-    nixpkgs.url = "github:NixOS/nixpkgs/21.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/22.11";
 
     utils.url = "github:numtide/flake-utils";
-    utils.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: inputs.utils.lib.eachSystem [
     # Add the system/architecture you would like to support here. Note that not
     # all packages in the official nixpkgs support all platforms.
     "x86_64-linux" "i686-linux" "aarch64-linux" "x86_64-darwin"
-  ] (system: let pkgs = import nixpkgs {
-                   inherit system;
+  ] (system: let
+    pkgs = import nixpkgs {
+      inherit system;
 
-                   # Add overlays here if you need to override the nixpkgs
-                   # official packages.
-                   overlays = [];
-                   
-                   # Uncomment this if you need unfree software (e.g. cuda) for
-                   # your project.
-                   #
-                   # config.allowUnfree = true;
-                 };
-             in {
-               devShell = pkgs.mkShell rec {
-                 # Update the name to something that suites your project.
-                 name = "my-c++-project";
+      # Add overlays here if you need to override the nixpkgs
+      # official packages.
+      overlays = [];
+      
+      # Uncomment this if you need unfree software (e.g. cuda) for
+      # your project.
+      #
+      # config.allowUnfree = true;
+    };
+  in {
+    devShells.default = pkgs.mkShell rec {
+      # Update the name to something that suites your project.
+      name = "my-c++-project";
 
-                 packages = with pkgs; [
-                   # Development Tools
-                   llvmPackages_11.clang
-                   cmake
-                   cmakeCurses
+      packages = with pkgs; [
+        # Development Tools
+        llvmPackages_14.clang
+        cmake
+        cmakeCurses
 
-                   # Development time dependencies
-                   gtest
+        # Development time dependencies
+        gtest
 
-                   # Build time and Run time dependencies
-                   spdlog
-                   abseil-cpp
-                 ];
+        # Build time and Run time dependencies
+        spdlog
+        abseil-cpp
+      ];
 
-                 # Setting up the environment variables you need during
-                 # development.
-                 shellHook = let
-                   icon = "f121";
-                 in ''
-                    export PS1="$(echo -e '\u${icon}') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
-                 '';
-               };
+      # Setting up the environment variables you need during
+      # development.
+      shellHook = let
+        icon = "f121";
+      in ''
+        export PS1="$(echo -e '\u${icon}') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
+      '';
+    };
 
-               defaultPackage = pkgs.callPackage ./default.nix {};
-             });
+    packages.default = pkgs.callPackage ./default.nix {};
+  });
 }
